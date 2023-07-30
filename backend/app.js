e71eb254b1e77f8e1413e6280a3db63462e9a768
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-07-30 22:01:21 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const NotFoundError = require('./utils/errors/notFoundError');
 
-const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3001, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
