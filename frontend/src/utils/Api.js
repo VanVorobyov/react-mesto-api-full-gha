53fc5420be8942aa4d463b77da528173584fb1dd
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-07-31 22:32:30 +0300

    Update Api.js

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -76,10 +76,9 @@ class Api {
 
 const api = new Api({
   baseUrl: 'https://api.vanvorobyov.nomoreparties.co',
-  // headers: {
-  //   authorization: 'e2a854ae-97cb-44db-b5bb-1f03cec72045',
-  //   'Content-Type': 'application/json',
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
