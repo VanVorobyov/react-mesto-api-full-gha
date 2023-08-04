f4a492149e0d46a2caeaaa9c9cb227018bb5a9ce
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-08-04 19:44:19 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const NotFoundError = require('./utils/errors/notFoundError');
 
-const { PORT, MONGODB } = process.env;
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 app.use(cors());
